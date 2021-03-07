a6deaea58cc94a1067dc6d6aeb00d5f230f8b908
K Ryan <hunt.kelsearyan@gmail.com>
2021-03-07 12:18:06 -0700

    Got the start quiz button to start timer, display first question

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,26 @@
-var questionOne = "Commonly used data types DO NOT include:"
-var questionOneAnswers {
-    
+var viewHS;
+var timer;
+var secondsAllowed = 60;
+var gameTimer = document.getElementById("gameTimer");
+var startQuizBtn = document.querySelector(".startQuizBtn");
+
+
+function startQuiz() {
+    setTimer();
 }
+
+// Sets the timer and produces the first question
+function setTimer() {
+    timer = setInterval(function() {
+        secondsAllowed--;
+        gameTimer.textContent = secondsAllowed;
+        if (secondsAllowed === 0) {
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+
+    document.getElementById('questionOne').style.display = 'block';
+    document.getElementById('rules').style.display = 'none';
+}
+
+startQuizBtn.addEventListener("click", startQuiz);
