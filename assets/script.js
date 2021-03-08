368c37839b90d874468d17e45c503c7016005928
K Ryan <hunt.kelsearyan@gmail.com>
2021-03-07 22:14:22 -0700

    Still unable to proceed to the next question

--- assets/script.js
+++ assets/script.js
@@ -5,18 +5,10 @@ var points;
 var gameTimer = document.getElementById("gameTimer");
 var startQuizBtn = document.querySelector(".startQuizBtn");
 var correctChoice = document.getElementById("correctChoice");
-var incorrectChoice = document.getElementById("incorrectChoice");
-
-function startQuiz() {
-  setTimer();
-  questionTwo();
-  questionThree();
-  questionFour();
-  questionFive();
-}
+var incorrectChoice = document.getElementsByClassName(".incorrectChoice");
 
 // Sets the timer and produces the first question
-function setTimer() {
+function startQuiz() {
   timer = setInterval(function () {
     secondsAllowed--;
     gameTimer.textContent = secondsAllowed;
@@ -31,78 +23,41 @@ function setTimer() {
 function questionOne() {
   document.getElementById("questionOne").style.display = "block";
   document.getElementById("rules").style.display = "none";
-
-  if (correctChoice.addEventListener("click", correctAlertMessage)) {
-    document.getElementById("correctAlert").style.display = "block";
-  }
-
-  if (incorrectChoice.addEventListener("click", incorrectAlertMessage)) {
-    document.getElementById("incorrectAlert").style.display = "block";
-  }
 }
 
 function questionTwo() {
-  setTimeout(2000);
   document.getElementById("questionTwo").style.display = "block";
   document.getElementById("questionOne").style.display = "none";
-
-  if (correctChoice.addEventListener("click", correctAlertMessage)) {
-    document.getElementById("correctAlert").style.display = "block";
-  }
-
-  if (incorrectChoice.addEventListener("click", incorrectAlertMessage)) {
-    document.getElementById("incorrectAlert").style.display = "block";
-  }
 }
 
 function questionThree() {
   document.getElementById("questionThree").style.display = "block";
   document.getElementById("questionTwo").style.display = "none";
-  if (correctChoice.addEventListener("click", correctAlertMessage)) {
-    document.getElementById("correctAlert").style.display = "block";
-  }
-
-  if (incorrectChoice.addEventListener("click", incorrectAlertMessage)) {
-    document.getElementById("incorrectAlert").style.display = "block";
-  }
 }
 
 function questionFour() {
   document.getElementById("questionFour").style.display = "block";
   document.getElementById("questionThree").style.display = "none";
-  if (correctChoice.addEventListener("click", correctAlertMessage)) {
-    document.getElementById("correctAlert").style.display = "block";
-  }
-
-  if (incorrectChoice.addEventListener("click", incorrectAlertMessage)) {
-    document.getElementById("incorrectAlert").style.display = "block";
-  }
 }
 
 function questionFive() {
   document.getElementById("questionFive").style.display = "block";
   document.getElementById("questionFour").style.display = "none";
-  if (correctChoice.addEventListener("click", correctAlertMessage)) {
-    document.getElementById("correctAlert").style.display = "block";
-  }
-
-  if (incorrectChoice.addEventListener("click", incorrectAlertMessage)) {
-    document.getElementById("incorrectAlert").style.display = "block";
-  }
 }
 
+function endGame() {
+    
+}
+
+// Alerts that pop up when user picks the correct or incorrect answer.
 function correctAlertMessage() {
   document.getElementById("correctAlert").style.display = "block";
-  setTimeout(2000);
 }
 
 function incorrectAlertMessage() {
   document.getElementById("incorrectAlert").style.display = "block";
   secondsAllowed -= 5;
   document.getElementById("gameTimer").innerHTML = secondsAllowed;
-  setTimeout(2000);
 }
 
 startQuizBtn.addEventListener("click", startQuiz);
-correctChoice.addEventListener("click", correctAlertMessage);
-incorrectChoice.addEventListener("click", incorrectAlertMessage);
