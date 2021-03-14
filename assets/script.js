94e16a9594aa483dfaf28955302d4208874d4558
K Ryan <hunt.kelsearyan@gmail.com>
2021-03-14 10:18:58 -0600

    Everything but the LOCALSTORAGE IS WORKING

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var viewHS;
 var timer;
-var secondsAllowed = 5;
+var secondsAllowed = 60;
 var results;
 var score = 0;
 var grade = document.getElementById("grade");
@@ -10,11 +10,12 @@ var correctChoice = document.getElementById("correctChoice");
 var incorrectChoice = document.getElementsByClassName(".incorrectChoice");
 
 // Sets the timer and produces the first question
+
 function startQuiz() {
   timer = setInterval(function () {
     secondsAllowed--;
     gameTimer.textContent = secondsAllowed;
-    if (secondsAllowed === 0) {
+    if (secondsAllowed <= 0) {
       clearInterval(timer);
       resultScreen();
     }
@@ -30,15 +31,14 @@ function questionOne() {
 
 function correctAlertMessage1() {
   document.getElementById("correctAlert1").style.display = "block";
-  setTimeout(questionTwo, "1000");
+  setTimeout(questionTwo, "500");
   score += 20;
 }
 
 function incorrectAlertMessage1() {
   document.getElementById("incorrectAlert1").style.display = "block";
   secondsAllowed -= 5;
-  document.getElementById("gameTimer").innerHTML = secondsAllowed;
-  setTimeout(questionTwo, "1000");
+  setTimeout(questionTwo, "500");
 }
 
 //Question Two
@@ -49,14 +49,14 @@ function questionTwo() {
 
 function correctAlertMessage2() {
   document.getElementById("correctAlert2").style.display = "block";
-  setTimeout(questionThree, "1000");
+  setTimeout(questionThree, "500");
   score += 20;
 }
 
 function incorrectAlertMessage2() {
   document.getElementById("incorrectAlert2").style.display = "block";
   secondsAllowed -= 5;
-  setTimeout(questionThree, "1000");
+  setTimeout(questionThree, "500");
 }
 // Question Three
 function questionThree() {
@@ -66,14 +66,14 @@ function questionThree() {
 
 function correctAlertMessage3() {
   document.getElementById("correctAlert3").style.display = "block";
-  setTimeout(questionFour, "1000");
+  setTimeout(questionFour, "500");
   score += 20;
 }
 
 function incorrectAlertMessage3() {
   document.getElementById("incorrectAlert3").style.display = "block";
   secondsAllowed -= 5;
-  setTimeout(questionFour, "1000");
+  setTimeout(questionFour, "500");
 }
 
 // Question Four
@@ -84,14 +84,14 @@ function questionFour() {
 
 function correctAlertMessage4() {
   document.getElementById("correctAlert4").style.display = "block";
-  setTimeout(questionFive, "1000");
+  setTimeout(questionFive, "500");
   score += 20;
 }
 
 function incorrectAlertMessage4() {
   document.getElementById("incorrectAlert4").style.display = "block";
   secondsAllowed -= 5;
-  setTimeout(questionFive, "1000");
+  setTimeout(questionFive, "500");
 }
 
 // Question Five
@@ -102,28 +102,49 @@ function questionFive() {
 
 function correctAlertMessage5() {
   document.getElementById("correctAlert5").style.display = "block";
-  setTimeout(resultScreen, "1000");
+  setTimeout(resultScreen, "500");
   score += 20;
 }
 
 function incorrectAlertMessage5() {
   document.getElementById("incorrectAlert5").style.display = "block";
   secondsAllowed -= 5;
-  setTimeout(resultScreen, "1000");
+  setTimeout(resultScreen, "500");
 }
 
+// Clean up code and consolidate. This is coded just to have a map of what this section needs to do.
 function resultScreen() {
   document.getElementById("inputInitialScreen").style.display = "block";
-  document.getElementById("questionOne").style.display = "none";
-  document.getElementById("questionTwo").style.display = "none";
-  document.getElementById("questionThree").style.display = "none";
-  document.getElementById("questionFour").style.display = "none";
   document.getElementById("questionFive").style.display = "none";
-  document.getElementById("rules").style.display = "none";
   results = setInterval(function () {
     score;
-    grade.textContent = score;
+    grade.textContent = score + secondsAllowed;
   });
+  clearInterval(timer);
 }
 
 startQuizBtn.addEventListener("click", startQuiz);
+
+
+var initialFormEl = document.getElementById("enterInitials");
+var userInput = document.getElementById('userInput');
+var playerListEl = document.getElementById("player-list");
+var submitBtn = document.getElementsByClassName("submitBtn");
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  var playerName = {
+    initials: userInput.value,
+    score: grade.textContent,
+  };
+  playerListEl.append(userInput.value + " - " + grade.textContent + " POINTS");
+  localStorage.setItem("playerName", JSON.stringify(playerName));
+  
+  var userText = userInput.value.trim();
+
+  if (userText === "") {
+    return;
+  }
+}
+
+initialFormEl.addEventListener("submit", handleFormSubmit);
